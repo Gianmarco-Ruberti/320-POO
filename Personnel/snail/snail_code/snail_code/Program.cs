d5857cc0556e7fa660d817e5c7e1f870c94df1b3
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2025-08-29 11:14:35 +0200

    feat(snail): ajout du code pour snail
    fait avec mes connaissance actuel

---
+++ Personnel/snail/snail_code/snail_code/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int life = 50;
+            int vertical = 10;
+            int horizontal = 1;
+            Console.SetCursorPosition(horizontal, vertical);
+            Console.WriteLine("_@_<");
+            do 
+            {
+                Console.SetCursorPosition(horizontal, vertical);
+                Console.WriteLine("    ");
+                Console.SetCursorPosition(horizontal, vertical);
+                horizontal++;
+                Console.SetCursorPosition(horizontal, vertical);
+                Console.WriteLine("_@_<");
+                life--;
+                Thread.Sleep(100);
+            }while(life!=0);
+            Console.SetCursorPosition(horizontal, vertical);
+            Console.WriteLine("____");
+            */
+
+        }
+    }
+}
