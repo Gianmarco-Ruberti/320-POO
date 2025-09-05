93c8974be8d69358098392defde982e69c5f68a3
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2025-09-05 08:52:53 +0200

    feat(exo-parachute):ajout des dimensions de l'écran de jeu

---
+++ Personnel/02-02-Parachutes/exo-parachute/exo-parachute/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+    }
+    static class Config
+    {
+        public const int SCREEN_HEIGHT = 40;
+        public const int SCREEN_WIDTH = 150;
+    }
+}
