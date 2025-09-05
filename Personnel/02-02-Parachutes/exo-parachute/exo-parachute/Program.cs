5f2893bd1dace7bed7ef123179925274cb2e5364
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2025-09-05 09:01:35 +0200

    feat(parachute) : ajout de la classe Plane et dessin asci de l'avion

--- Personnel/02-02-Parachutes/exo-parachute/exo-parachute/Program.cs
+++ Personnel/02-02-Parachutes/exo-parachute/exo-parachute/Program.cs
@@ -7,11 +7,22 @@ namespace MyApp
         static void Main(string[] args)
         {
         }
-        
     }
     static class Config
     {
         public const int SCREEN_HEIGHT = 40;
         public const int SCREEN_WIDTH = 150;
+    }
+    class Plane
+    {
+        private string[] view =
+{
+            @" _                         ",
+            @"| \                        ",
+            @"|  \       ______          ",
+            @"--- \_____/  |_|_\____  |  ",
+            @"  \_______ --------- __>-} ",
+            @"        \_____|_____/   |  "
+        };
     }
 }
